fc631bd3b08a8b858a9c48607f21635575be4e64
unknown <pedrorembold@gmail.com>
2022-09-12 12:07:07 -0300

    JS criado no botão do saiba mais, classes alteradas, precisa ainda de refino

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
-const botaoSaibaMais = document.querySelector(".btn-saiba_mais");
-botaoSaibaMais.addEventListener('click', abreAba())
+var botaoSaibaMais = document.querySelector(".btn-saiba_mais");
+var descricao = document.querySelector(".descricao-projetos");
 
-function abreAba(){
-    abaAberta
-}
+botaoSaibaMais.addEventListener('click', function(){
+    
+    if(descricao.style.display === 'block'){
+        descricao.style.display = 'none';
+        botaoSaibaMais.innerHTML = "Saiba Mais"
+        botaoSaibaMais.classList.remove("btn-saiba_mais_ativo");
+    } else {
+        descricao.style.display = 'block';
+        botaoSaibaMais.innerHTML = "Saiba Menos";
+        botaoSaibaMais.classList.add("btn-saiba_mais_ativo");
+        
+    }});
+
