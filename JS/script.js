9e5efc4b3d64a5bc7707d778214b33a522893a07
unknown <pedrorembold@gmail.com>
2022-09-15 10:58:40 -0300

    JS melhorado nas seçoes, mas ainda orecisa de reparo, as decriçoes nao condizem

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,27 @@
-var botaoSaibaMais = document.querySelector(".btn-saiba_mais");
+var botaoSaibaMais = document.querySelectorAll(".btn-saiba_mais");
 var descricao = document.querySelector(".descricao-projetos");
 
-botaoSaibaMais.addEventListener('click', function(){
-    
-    if(descricao.style.display === 'block'){
-        descricao.style.display = 'none';
-        botaoSaibaMais.innerHTML = "Saiba Mais"
-        botaoSaibaMais.classList.remove("btn-saiba_mais_ativo");
-    } else {
-        descricao.style.display = 'block';
-        botaoSaibaMais.innerHTML = "Saiba Menos";
-        botaoSaibaMais.classList.add("btn-saiba_mais_ativo");
-        
-    }});
-    
+
+for(let i = 0; i < botaoSaibaMais.length; i++) {
+    const botao = botaoSaibaMais[i]
+
+    botao.onclick = function(){
+
+            if(descricao.style.display === 'block'){
+            descricao.style.display = 'none';
+            botao.innerHTML = "Saiba Mais";
+            botao.classList.remove("btn-saiba_mais_ativo");
+        } else {
+            descricao.style.display = 'block';
+            botao.innerHTML = "Saiba Menos";
+            botao.classList.add("btn-saiba_mais_ativo");
+
+        }};
+}
+
+
+
+
+
+
 
