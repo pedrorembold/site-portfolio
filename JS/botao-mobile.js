3e8cb3d5ed2acfe09f1f66daae3e08dbb73a86da
unknown <pedrorembold@gmail.com>
2022-09-28 18:11:47 -0300

    JS do cabeçalho ajustado. botão funcionando

---
+++ JS/botao-mobile.js
@@ -0,0 +1,10 @@
+const botaoMobile = document.querySelector(".botao-mobile");
+
+botaoMobile.addEventListener('click', function(){
+    let menuMobile = document.querySelector('.menu-mobile')
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open')
+    }else{
+        menuMobile.classList.add('open')
+    }
+});
